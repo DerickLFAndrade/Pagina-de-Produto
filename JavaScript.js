ce5c793d0b7bbf4594290c63065dbac9352d2934
DerickLFAndrade <derickspy15@gmail.com>
2021-10-09 04:38:58 -0300

    Criação nav mobile

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,12 @@
 var navBot = document.getElementById('navBot')
 var divFiltro = document.getElementById('divFiltro')
 var fecharFiltro = document.getElementById('fecharFiltro')
+var containerMaster = document.getElementById('containerMaster')
+
 navBot.addEventListener('click', function() {
+    containerMaster.style.opacity = '0.1'
+    
+    
     divFiltro.style.visibility = 'visible'
     fecharFiltro.addEventListener('click', function() {
         divFiltro.style.visibility = 'hidden'
