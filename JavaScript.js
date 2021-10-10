06b4ace31357d969ffc9c517eed7b589980f6470
DerickLFAndrade <derickspy15@gmail.com>
2021-10-10 01:25:04 -0300

    Finalização design carrinho

--- JavaScript.js
+++ JavaScript.js
@@ -2,7 +2,17 @@ var navBot = document.getElementById('navBot')
 var divFiltro = document.getElementById('divFiltro')
 var fecharFiltro = document.getElementById('fecharFiltro')
 var containerMaster = document.getElementById('containerMaster')
+var carrinhoNav = document.getElementById('carrinhoNav')
+var itensCarrinho = document.getElementById('itensCarrinho')
 
+carrinhoNav.addEventListener('click', function(){
+    if(itensCarrinho.style.visibility == 'hidden') {
+        itensCarrinho.style.visibility = 'visible'
+    } else {
+        itensCarrinho.style.visibility = 'hidden'
+    }
+   
+})
 navBot.addEventListener('click', function() {
     containerMaster.style.opacity = '0.1'
     
