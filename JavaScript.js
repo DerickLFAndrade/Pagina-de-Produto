5923f95ee8946e0311fbe3242f2bbdc6c10bf829
DerickLFAndrade <derickspy15@gmail.com>
2021-10-11 03:38:30 -0300

    Atualização elementos

--- JavaScript.js
+++ JavaScript.js
@@ -3,16 +3,21 @@ var divFiltro = document.getElementById('divFiltro')
 var fecharFiltro = document.getElementById('fecharFiltro')
 var containerMaster = document.getElementById('containerMaster')
 var carrinhoNav = document.getElementById('carrinhoNav')
+var botAdd = document.getElementById('botAdd')
 var itensCarrinho = document.getElementById('itensCarrinho')
+var produtoCarrinho = document.getElementById('produtoCarrinho')
 var botMais = document.getElementById('mais')
 var botMenos = document.getElementById('menos')
 var quantidade = document.getElementById('quantidade')
+var quantidadeCar = document.getElementById('quantidadeCar')
+var somaValor 
 botMenos.addEventListener('click', remove)
 botMais.addEventListener('click', add)
 function add() {
    if(quantidade.value >= 0 ){
-        quantidade.value++
-     
+      
+      quantidadeCar =  quantidade.value++
+      
   }
 }
 function remove() {
@@ -22,14 +27,40 @@ function remove() {
     }
 
 }
+
 carrinhoNav.addEventListener('click', function(){
     if(itensCarrinho.style.visibility == 'hidden') {
         itensCarrinho.style.visibility = 'visible'
+        
+
     } else {
         itensCarrinho.style.visibility = 'hidden'
     }
    
 })
+
+
+botAdd.addEventListener('click', function () {
+    let preçoCar = document.getElementById('preçoCar')
+    let empty = document.getElementById('empty').style.visibility = 'hidden'
+    let resultado = document.getElementById('resultado')
+    if (produtoCarrinho.style.visibility == 'hidden'){
+        produtoCarrinho.style.visibility = 'visible'
+        itensCarrinho.style.visibility = 'visible'
+   
+    } else {
+        produtoCarrinho.style.visibility = 'hidden'
+        itensCarrinho.style.visibility = 'hidden'
+    }
+    
+    
+    
+    somaValor += quantidadeCar * Number(preçoCar.value)
+    console.log(somaValor)
+  })
+
+
+
 navBot.addEventListener('click', function() {
     containerMaster.style.opacity = '0.1'
     
